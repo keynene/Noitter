78064090d74e48267af6d9c46e1fb3cac3b22d79
keynene <nonung2@naver.com>
2023-01-26 16:46:55 +0900

    Update path 경로수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import AppRouter from './Router';
+/* eslint-disable */ //warning 제거
+import React, { useState } from 'react';
+/* 기존경로 변경 (jsconfig.json에서 src로 기본경로 지정해놓음) */
+import AppRouter from 'components/Router';
+import { authService } from 'fbase';
 
 
 function App() {
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
   return (
-    <AppRouter />
+    <div>
+      <AppRouter isLoggedIn={ isLoggedIn }/>
+      <footer>&copy; {new Date().getFullYear()} Twitter_clone</footer>
+    </div>
+
   );
 }
 
