026baa60c2ec69c19579cb8ef8c525572af38a77
keynene <nonung2@naver.com>
2023-01-26 15:39:04 +0900

    Rename firebase to fbase, Create jsconfig.json (jsconfig = 기존경로 src로 변경)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import firebase from './firebase.js';
+import firebase from 'firebase/compat/app';
 import { BrowserRouter } from 'react-router-dom';
 console.log(firebase)
 
