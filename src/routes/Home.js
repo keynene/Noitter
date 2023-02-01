7d53a881629241d6dd26cf6a42dd353685d65d79
keynene <nonung2@naver.com>
2023-02-01 18:48:11 +0900

    Create database (firebase)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home =  () => <span>Home</span>
+const Home =  () => {
+	const [noweet, setNoweet] = useState("");
+	const onSubmit = (e) => {
+		e.preventDefault();
+	}
+	const onChange = (e) => {
+		// setNoweet(e.target.value)  : 똑같이 동작하는데 왜 이렇게 안쓰지? value 말고 여러 값 저장하려면 es6 문법이 편해서 그런가
+		const {
+			target: { value }
+		} = e;
+		setNoweet(value);
+		console.log(noweet)
+	}
+	return(
+		<div>
+			<form onSubmit={ onSubmit }>
+				<input value={ noweet } onChange={ onChange } type="text" placeholder="'What's on your mind?" maxLength={120} />
+				<input type="submit" value="Noweet" />
+			</form>
+		</div>
+	)
+}
 
 export default Home;
